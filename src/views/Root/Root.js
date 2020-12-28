f861c0641b0327f434e557997c53c69b32ecc804
Justyna Krzywda <justyna.krzywda@gmail.com>
2020-12-28 18:20:40 +0000

    global styles

--- src/views/Root/Root.js
+++ src/views/Root/Root.js
@@ -1,14 +1,18 @@
  import Button from "../../components/Button/Button"
+ import GlobalStyle from "../../components/theme/GlobalStyle"
 
 function Root() {
   return (
-    <div className="App">
+    <div>
+      <GlobalStyle>
      <div>
        Hello
      </div>
      <Button width="500px">CLOSE / SAVE</Button>
      <Button secondary>Remove</Button>
+    </GlobalStyle>
     </div>
+    
   );
 }
 
