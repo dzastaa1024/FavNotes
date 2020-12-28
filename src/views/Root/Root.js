aa75becdde1b4e9a6036261f96f36ff7fcea1069
Justyna Krzywda <justyna.krzywda@gmail.com>
2020-12-28 17:49:08 +0000

    button component

---
+++ src/views/Root/Root.js
@@ -0,0 +1,17 @@
+ import Button from "../../components/Button/Button"
+
+function Root() {
+  return (
+    <div className="App">
+     <div>
+       Hello
+     </div>
+     <Button width="500px">CLOSE / SAVE</Button>
+     <Button secondary>Remove</Button>
+    </div>
+  );
+}
+
+export default Root;
+
+
