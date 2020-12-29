909de2348b0fb43e6324b8c2fb214a11bf0934c3
Justyna Krzywda <justyna.krzywda@gmail.com>
2020-12-29 18:15:08 +0000

    global styles

--- src/views/Root/Root.js
+++ src/views/Root/Root.js
@@ -1,16 +1,22 @@
- import Button from "../../components/Button/Button"
- import GlobalStyle from "../../components/theme/GlobalStyle"
+ import Button from "../../components/atoms/Button/Button";
+ import GlobalStyle from "../../components/theme/GlobalStyle";
+ import { ThemeProvider } from "styled-components";
+ import { theme } from "../../components/theme/mainTheme";
+
 
 function Root() {
   return (
     <div>
-      <GlobalStyle>
-     <div>
-       Hello
-     </div>
+      <GlobalStyle />
+      <ThemeProvider theme={theme}>
+        <>
+      <div>
+        Hello
+      </div>
      <Button width="500px">CLOSE / SAVE</Button>
      <Button secondary>Remove</Button>
-    </GlobalStyle>
+     </>
+    </ThemeProvider> 
     </div>
     
   );
